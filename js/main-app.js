da5c412575eb8018673132228a36a174d71fa4ae
RANA <wrana697@gmail.com>
2022-11-24 02:17:08 +0600

    App updated!

--- js/main-app.js
+++ js/main-app.js
@@ -9,8 +9,42 @@ function getPinCode(){
         // return pinCode;
     }
 }
+// display the pin code
 function displayPinNumber(){
     const disPlayPin = getPinCode();
     document.getElementById('display-pin').value = disPlayPin;
+}
 
+// add number in the calc keypad box!
+document.getElementById('calculator-keypad').addEventListener('click', function(event){
+    const number = event.target.innerText;
+    const calcInput = document.getElementById('numbers-typed');
+    if(isNaN(number)){
+        if(number == 'C'){
+            calcInput.value = '';
+        }
+    }
+    else{
+        const previousNumber = calcInput.value;
+        const newNumber = previousNumber + number;
+        calcInput.value = newNumber;
+    }
+});
+
+// show success or fail message!
+function verifyPinCode(){
+    const currentPin = document.getElementById('display-pin').value;
+    const numberTyped = document.getElementById('numbers-typed').value;
+
+    const notifySuccess = document.getElementById('notify-success');
+    const failedError = document.getElementById('notify-fail');
+
+    if(currentPin == numberTyped){
+        notifySuccess.style.display = 'block';
+        failedError.style.display = 'none';
+    }
+    else{
+        failedError.style.display = 'block';
+        notifySuccess.style.display = 'none';
+    }
 }
